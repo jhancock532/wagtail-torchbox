9f889993edcdbe9ebfb8e5cc992daf61d1694fed
William <william.blackie@torchbox.com>
2021-11-12 15:56:43 +0000

    Alter image relations to new model

--- tbx/images/migrations/0002_copy_custom_images.py
+++ tbx/images/migrations/0002_copy_custom_images.py
@@ -1,12 +1,13 @@
 from django.db import migrations
 
+
 def copy_images(apps, schema_editor):
     # Here we copy and initialise our new image models and their renditions
-    CustomImage = apps.get_model('images', 'CustomImage')
-    Rendition = apps.get_model('images', 'Rendition')
+    CustomImage = apps.get_model("images", "CustomImage")
+    Rendition = apps.get_model("images", "Rendition")
 
-    TorchboxImage = apps.get_model('torchbox', 'TorchboxImage')
-    TorchboxRendition = apps.get_model('torchbox', 'TorchboxRendition')
+    TorchboxImage = apps.get_model("torchbox", "TorchboxImage")
+    TorchboxRendition = apps.get_model("torchbox", "TorchboxRendition")
 
     # Copy old images to new model
     for tbx_image in TorchboxImage.objects.values():
@@ -18,11 +19,12 @@ def copy_images(apps, schema_editor):
         rendition = Rendition.objects.create(**tbx_rendition)
         rendition.save()
 
+
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('images', '0001_initial'),
-        ('torchbox', '0139_homepage_featured_posts_and_hero_image')
+        ("images", "0001_initial"),
+        ("torchbox", "0139_homepage_featured_posts_and_hero_image"),
     ]
 
     operations = [
